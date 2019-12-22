2e9a3687cde2508f357bc20454cf2e8cf3df67b7
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-22 14:53:40 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment
+  attr_accessor :patient, :doctor 
+  
+  @@all = []
+  
+  def initialize(patient, doctor)
+    @name = name
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
