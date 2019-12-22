672913d9b9f21e4ce3ba318d6145f3c99da41d17
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-22 14:55:25 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -14,3 +14,8 @@ class Appointment
   end
   
 end
+
+ferris = Patient.new("Ferris Bueller")
+doogie = Doctor.new("Doogie Howser")
+
+new_appointment = Appointment.new('January 1st, 1989', ferris, doogie)
